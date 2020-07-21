b8564fd78c0ffdee8e9d4f498bce3900117a1343
Yonatan Dafna <yonatandafna1@gmail.com>
2020-07-21 16:26:44 +0300

    Things

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,7 +8,7 @@ module.exports = class extends Generator {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the transcendent ${chalk.red(
+        `Welcome to the awesome ${chalk.red(
           "generator-minecraft-datapack"
         )} generator!`
       )
@@ -16,10 +16,42 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
+        type: "input",
+        name: "name",
+        message: "Datapack name:"
+      },
+      {
+        type: "input",
+        name: "namespace",
+        message:
+          "Choose a namespace (This must be unique, and only contain alphanumeric and underscores!):"
+      },
+      {
+        type: "list",
+        name: "format",
+        message: "Please choose a pack format (5 for 1.15+):",
+        default: 5,
+        choices: [1, 2, 3, 4, 5]
+      },
+      {
+        type: "input",
+        name: "description",
+        message:
+          "Enter a pack descrption (This will be used to describe your pack lol):"
+      },
+      {
+        type: "input",
+        name: "main",
+        message:
+          "Main function name (This function will be called every tick):",
+        default: "main"
+      },
+      {
+        type: "input",
+        name: "load",
+        message:
+          "Load function name (This function will be called when loading or reloading a world):",
+        default: "load"
       }
     ];
 
@@ -30,13 +62,43 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
+    const props = this.props;
+    const pack = {
+      pack_format: props.format,
+      description: props.description
+    };
+    const tick = {
+      replace: false,
+      values: [props.namespace + ":" + props.main]
+    };
+    const load = {
+      replace: false,
+      values: [props.namespace + ":" + props.load]
+    };
+    this.fs.writeJSON(this.destinationPath("pack.mcmeta"), { pack: pack });
+    this.fs.writeJSON(
+      this.destinationPath("data/minecraft/tags/functions/load.json"),
+      load
+    );
+    this.fs.writeJSON(
+      this.destinationPath("data/minecraft/tags/functions/tick.json"),
+      tick
+    );
+    this.fs.write(
+      this.destinationPath(
+        "data/" + props.name + "/functions/" + props.load + ".mcfunction"
+      ),
+      "# Insert your commands here - This will be called on a world load/reload\n\nsay The " +
+        props.name +
+        " Datapack successfully reloaded!\n"
+    );
+    this.fs.write(
+      this.destinationPath(
+        "data/" + props.name + "/functions/" + props.main + ".mcfunction"
+      ),
+      "# Insert your commands here - This will be called every tick (20 time a second)\n"
     );
   }
 
-  install() {
-    this.installDependencies();
-  }
+  install() {}
 };
